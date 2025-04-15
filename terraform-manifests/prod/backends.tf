8723980e526a7e25f1321b4e95c1d8e648dcb09d
pranavvarne11 <pranavvarne11@gmail.com>
2025-04-15 10:30:07 +0530

    commit tf-test

---
+++ terraform-manifests/prod/backends.tf
@@ -0,0 +1,17 @@
+# terraform {
+#   cloud {
+#     organization = "ORGNAME"
+
+#     workspaces {
+#       name = "dev_tf_resources_ws"
+#     }
+#   }
+# }
+
+terraform {
+  backend "s3" {
+    bucket = "aws-devops-testbucket"
+    key    = "terraformstates/prod.tfstate"
+    region = "us-east-1"
+  }
+}
